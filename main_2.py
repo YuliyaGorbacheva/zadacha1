b6b2ca26887f623602cf72f42db9948c8c5ebb93
YuliyaGorbacheva <Ulechka782@yandex.ru>
2024-09-13 11:27:13 +0400

    main_2.py

--- main_2.py
+++ main_2.py
@@ -1,5 +1,5 @@
-numbers_completed_homework='12'
-numbers_spent_hours='1.5'
+numbers_completed_homework=12
+numbers_spent_hours=1.5
 course_name='Pyton'
-time_for_one_task='0.125'
+time_for_one_task=numbers_spent_hours/numbers_completed_homework
 print('Курс:',course_name,', всего задач:',numbers_completed_homework,', затрачено часов:',numbers_spent_hours, ', среднее время выполнения:',time_for_one_task,'часа.')
