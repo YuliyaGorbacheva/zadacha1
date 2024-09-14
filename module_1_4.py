a0c02da14e85bfa023defa6bc4808146ae0ea9ca
YuliyaGorbacheva <Ulechka782@yandex.ru>
2024-09-14 14:23:26 +0400

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string=input()          #2
+print(my_string)
+print(my_string.upper())   #3
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])        #4
+print(my_string[-1])
