856107cd6f2a032726d5cdcaa0839a7996310c1c
YuliyaGorbacheva <Ulechka782@yandex.ru>
2024-09-15 13:46:07 +0400

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var=tuple_=1,2,'s','v'   #2
+print(immutable_var)
+
+#immutable_var=tuple_[1]='b'  #3
+#print(immutable_var)
+
+mutable_list=[1,'v','s',4,5]   #4
+mutable_list[1]=2
+mutable_list[-2]='m'
+print(mutable_list)
