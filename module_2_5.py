78ac2b4ef45075c882c33a3d1c1dc80b7bf100c0
YuliyaGorbacheva <Ulechka782@yandex.ru>
2024-09-25 22:05:06 +0400

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -1,9 +1,13 @@
 def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
-        matrix.append([])
-        for j in range(m):
-            matrix[i].append(value)
+        if n > 0 and m > 0:
+            matrix.append([])
+            for j in range(m):
+                matrix[i].append(value)
+        else:
+            matrix = []
+
     print(matrix)
 
 
